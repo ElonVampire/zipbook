2c79785497ba47048ad2a6ff1a7bf6e0a991bbcb
limodou <limodou@64d65547-8d32-0410-8dd2-e3966cd06553>
2007-06-12 09:27:58 +0000

    修正bug

--- trunk/apps/zipbooks/views.py
+++ trunk/apps/zipbooks/views.py
@@ -8,16 +8,40 @@ from utils.lib_page import Page
 from django.conf import settings
 import os, sys
 import traceback
+from plugins import tools
 
 def index(request):
     return render_template(request, 'zipbooks/index.html')
 
-def add(request):
-    url = request.POST.get('url', '')
+def add(request, book_id=None):
+    if book_id:
+        try:
+            book = Book.objects.get(pk=int(book_id))
+        except Book.DoesNotExist:
+            return ajax_fail(message="此书在数据库中不存在！")
+        url = book.url
+    else:
+        url = request.POST.get('url', '')
     if url:
-        os.spawnl(os.P_NOWAIT, sys.executable, 'python', os.path.abspath(settings.CMD), 'get', url)
+        args = list(settings.PARAMS) + ['get', url]
+        try:
+           os.spawnl(*args)
+        except:
+            traceback.print_exc()
         return ajax_ok(message="任务已经提交！请稍候下载。")
     return ajax_fail(message="请输入想下载书的目录URL")
+
+def refresh(request, book_id):
+    try:
+        book = Book.objects.get(pk=int(book_id))
+    except Book.DoesNotExist:
+        return ajax_fail(message="此书在数据库中不存在！")
+    args = list(settings.PARAMS) + ['reget', book.url]
+    try:
+       os.spawnl(*args)
+    except:
+        traceback.print_exc()
+    return ajax_ok(message="任务已经提交！请稍候下载。")
         
 def booklist(request):
     s = []
@@ -30,6 +54,21 @@ def booklist(request):
     cur = int(request.GET.get('page', 1))
     return ajax_ok((cur, pages, s))
 
+def _get_content(book_id, path, o):
+    if settings.CONTENT == 'db':
+        return o.content
+    else:
+        filename = os.path.join(path, str(book_id), "%04d.txt" % o.order)
+        if os.path.exists(filename):
+            try:
+                f = file(filename, 'rb')
+                text = f.read()
+                f.close()
+                return text
+            except:
+                traceback.print_exc()
+        return ''
+    
 def download(request, book_id):
     from utils.zfile import ZFile
     import os
@@ -44,28 +83,16 @@ def download(request, book_id):
     if request.GET.get('onefile', ''):
         c = []
         for o in book.chapter_set.all():
-            if settings.CONTENT == 'db':
-                c.append(o.content)
-            else:
-                filename = os.path.join(path, "%04d.txt" % o.order)
-                if os.path.exists(filename):
-                    fi = file(filename, 'rb')
-                    try:
-                        try:
-                            c.append(fi.read())
-                        except:
-                            traceback.print_exc()
-                            continue
-                    finally:
-                        fi.close()
-        f.addstring(unicode(book.name, 'utf-8').encode('gb18030', 'ignore') + '.txt', '\r\n\r\n'.join(c))
+            c.append(_get_content(book.id, path, o))
+        f.addstring(tools.to_encode(book.name) + '.txt', '\r\n\r\n'.join(c))
     else:
         for o in book.chapter_set.all():
-            f.addstring(("%04d" % o.order)+'.txt', o.content)
+            f.addstring(("%04d" % o.order)+'.txt', _get_content(book.id, path, o))
+                
     f.close()
     
     response = HttpResponse(mimetype='application/zip')
-    response['Content-Disposition'] = 'attachment; filename=%s.zip' % book.name
+    response['Content-Disposition'] = 'attachment; filename=%s.zip' % tools.to_encode(book.name)
     
     response.write(file(filename, 'rb').read())
     return response
